4db2207296b3c7258407f981e89fc79fc092ef4c
Sibansu Mishra <87473469+sibansu@users.noreply.github.com>
2022-07-09 00:03:57 +0530

    Implement strstr

---
+++ String/easy_seven.cpp
@@ -0,0 +1,79 @@
+/*
+// Implement strstr 
+
+Your task is to implement the function strstr. The function takes two strings as arguments (s,x) and  locates the occurrence of the string x in the string s. The function returns and integer denoting the first occurrence of the string x in s (0 based indexing).
+
+Note: You are not allowed to use inbuilt function.
+
+ 
+
+Example 1:
+
+Input:
+s = GeeksForGeeks, x = Fr
+Output: -1
+Explanation: Fr is not present in the
+string GeeksForGeeks as substring.
+ 
+
+Example 2:
+
+Input:
+s = GeeksForGeeks, x = For
+Output: 5
+Explanation: For is present as substring
+in GeeksForGeeks from index 5 (0 based
+indexing).
+ 
+
+Your Task:
+You don't have to take any input. Just complete the strstr() function which takes two strings str, target as an input parameter. The function returns -1 if no match if found else it returns an integer denoting the first occurrence of the x in the string s.
+
+ 
+
+Expected Time Complexity: O(|s|*|x|)
+Expected Auxiliary Space: O(1)
+
+Note : Try to solve the question in constant space complexity.
+
+ 
+
+Constraints:
+1 <= |s|,|x| <= 100
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int strstr(string ,string);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a;
+        string b;
+        
+        cin>>a;
+        cin>>b;
+        
+        cout<<strstr(a,b)<<endl;
+    }
+}
+
+int strstr(string s, string x)
+{
+     int m = s.length();
+     int n = x.length();
+     
+     for(int i=0;i<m;i++){
+         string h = s.substr(i,n);
+         if(h==x){
+             return i;
+             break;
+         }
+     }
+     return -1;
+}
