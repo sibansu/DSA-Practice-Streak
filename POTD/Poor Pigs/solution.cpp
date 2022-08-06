d85689d925f388dd16afcf457b4acf60df5a1124
Sibansu Mishra <87473469+sibansu@users.noreply.github.com>
2022-08-06 21:58:16 +0530

    Poor Pigs

---
+++ POTD/Poor Pigs/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        if(buckets==1)
+        return 0;
+    int digits = 1;
+    int base = minutesToTest / minutesToDie + 1;
+    int timer = base;
+    while(buckets>timer)
+    {
+        timer*=base;
+        digits+=1;
+    }
+    return digits;
+    }
+};
